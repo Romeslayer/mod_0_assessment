9502448a9f2afcdf14b30ecdd894a28fa73ea3a9
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-10-20 08:28:57 -0600

    Add DIA model to modelA

---
+++ models/modelA.js
@@ -0,0 +1,13 @@
+var denverAirport = {
+  name: "Denver International Airport",
+  address: "8500 Peña Blvd",
+  city: "Denver",
+  state: "Colorado",
+  zip: 80249,
+  hasFlights: true,
+  inBoundTrips:["F12","A37","Z72"],
+  outBoundTrips:["S91","S24","A19"],
+  maxOccupancy: 3000,
+  hasParking: true,
+
+}
