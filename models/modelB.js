6bbd845943ed67532428842d04405ec3e1c85f92
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-10-20 08:34:09 -0600

    Add mustang model to modelB

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var fordMustang = {
+  make: "Ford",
+  model: "Mustang",
+  year: 2007,
+  odometer: 110000,
+  citiesVisted: ["Monument", "Colorado Springs", "Denver"],
+  partsRepaired: ["engine", "altenator", "transmission", "oxygen sensor"]
+  engineLightOn: false,
+  hasGas: true,
+}
