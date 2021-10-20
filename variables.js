3f830197a491ed33015c4e2d06d2009121e5058d
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-10-20 08:21:02 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,16 @@
+var famousPerson = "Will Smith";
+
+var isActive = true;
+
+famousPerson = "Connor McGregor";
+var num3 = 3;
+var num9 = 9;
+var sum = num3 + num9;
+
+
+var animals = ['Penguin', 'Chicken', 'Walrus'];
+animals.shift();
+
+
+
+console.log(famousPerson.length)
