0b2389a4df26ec384b78f76e5c66aead2f1210dc
DéNaje Ferguson <denaje.ferguson@gmail.com>
2021-10-25 07:32:02 +0200

    Fix syntax error line 7

--- models/modelB.js
+++ models/modelB.js
@@ -4,7 +4,7 @@ var fordMustang = {
   year: 2007,
   odometer: 110000,
   citiesVisted: ["Monument", "Colorado Springs", "Denver"],
-  partsRepaired: ["engine", "altenator", "transmission", "oxygen sensor"]
+  partsRepaired: ["engine", "altenator", "transmission", "oxygen sensor"],
   engineLightOn: false,
   hasGas: true,
 }
